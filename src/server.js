08f86a20105d4b103f4ea3cedbf06161ce31a23a
Mani <manipatelhindu@gmail.com>
2025-08-28 12:34:10 +0530

    install mongoose

--- src/server.js
+++ src/server.js
@@ -12,3 +12,8 @@ app.use("/api/notes", notesRoutes);
 app.listen(5002, () => {
   console.log("✅ Server is running on port 5002");
 });
+
+
+export default app;
+
+// mongodb+srv://manideepkatkam83_db_user:1yQZgTEBU5SOIqvP@cluster0.ymnv1uh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
