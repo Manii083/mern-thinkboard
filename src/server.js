ec0b8b4692339ba2893f8bd19fd7dda97c7f4174
Mani <manipatelhindu@gmail.com>
2025-08-28 20:20:47 +0530

    mongoDB schema

--- src/server.js
+++ src/server.js
@@ -1,19 +1,25 @@
 import express from "express";
 import notesRoutes from "./Routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
+
+const PORT = process.env.PORT || 5002;
 
 // Middleware to parse JSON request bodies
 app.use(express.json());
 
+connectDB();
+
 // Mount the notes router
 app.use("/api/notes", notesRoutes);
 
-app.listen(5002, () => {
-  console.log("✅ Server is running on port 5002");
+app.listen(PORT, () => {
+  console.log("✅ Server is running on port", PORT);
 });
 
 
 export default app;
-
-// mongodb+srv://manideepkatkam83_db_user:1yQZgTEBU5SOIqvP@cluster0.ymnv1uh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
