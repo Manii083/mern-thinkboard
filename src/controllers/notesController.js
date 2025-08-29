d2e4434c2c967f4a9dcea22e68b4e7d0d111a286
Mani <manipatelhindu@gmail.com>
2025-08-29 17:44:38 +0530

    modify arrow function to normal functions

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,15 +1,15 @@
-export const getAllNotes = (req, res) => {
+export function getAllNotes(req, res) {
   res.status(201).json({ message: "Note fetched successfully" });
 };
 
-export const createNote = (req, res) => {
+export function createNote (req, res) {
   res.status(200).json({ message: "Note Created successfully" });
 }
 
-export const updateNote = (req, res) => {
+export function updateNote(req, res) {
   res.status(200).json({ message: "Note updated successfully" });
 }
 
-export const deleteNote = (req, res) => {
+export function deleteNote(req, res) {
   res.status(200).json({ message: "Note deleted successfully" });
 }
