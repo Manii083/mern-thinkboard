3ba3a1410e2bae91a82a5a3f4119a10f7aa95b81
Mani <manipatelhindu@gmail.com>
2025-09-09 18:41:59 +0530

    update  deploy code

--- Frontend/src/libs/axios.js
+++ Frontend/src/libs/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:5002/api" : "/api";
+  import.meta.env.MODE === "development"
+    ? "http://localhost:5002/api"
+    : "/api/notes"; // <-- update for production
 
 const api = axios.create({
   baseURL: BASE_URL,
