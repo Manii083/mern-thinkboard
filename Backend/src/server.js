866e98d51aedb898007ab31b9a4aee8a7d3c7640
Mani <manipatelhindu@gmail.com>
2025-09-09 16:59:59 +0530

    update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import path from "path";
+import { fileURLToPath } from "url"; // Fix __dirname for ES modules
 import notesRoutes from "./Routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middlewares/ratelimitter.js";
@@ -11,7 +12,10 @@ dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5002;
-const __dirname = path.resolve();
+
+// Fix __dirname for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Middleware to parse JSON request bodies
 if (process.env.NODE_ENV !== "production") {
@@ -24,24 +28,16 @@ if (process.env.NODE_ENV !== "production") {
 app.use(express.json());
 app.use(rateLimiter);
 
-// a simple middleware to log request method and url
-// app.use((req, res, next) => {
-//   console.log(`the request method is ${req.method} and the url is ${req.url}`);
-//   next();
-// });
-
 // Mount the notes router
 app.use("/api/notes", notesRoutes);
 
-app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
+// Serve static files
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 
-  app.get(/.*/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-});
-
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
+  });
 }
 
 connectDB().then(() => {
